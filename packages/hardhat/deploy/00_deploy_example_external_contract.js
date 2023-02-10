053681fb85117a767fdcec07cbb574e36f3697ec
sebaraba <rabasebastians@gmail.com>
2023-02-10 20:00:37 +0200

    fix deploy

--- packages/hardhat/deploy/00_deploy_example_external_contract.js
+++ packages/hardhat/deploy/00_deploy_example_external_contract.js
@@ -15,56 +15,6 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   });
 
   const externalContract = await ethers.getContract("ExternalContract");
-
-  // Getting a previously deployed contract
-  // const ExternalContract = await ethers.getContract(
-  //   "ExternalContract",
-  //   deployer
-  // );
-
-  // await YourContract.setPurpose("Hello");
-
-  // if you want to instantiate a version of a contract at a specific address!
-  // const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A")
-
-  // If you want to send value to an address from the deployer
-  // const deployerWallet = ethers.provider.getSigner()
-  // await deployerWallet.sendTransaction({
-  //   to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
-  //   value: ethers.utils.parseEther("0.001")
-  // })
-
-  // If you want to send some ETH to a contract on deploy (make your constructor payable!)
-  // const yourContract = await deploy("YourContract", [], {
-  // value: ethers.utils.parseEther("0.05")
-  // });
-
-  // If you want to link a library into your contract:
-  // reference: https://github.com/austintgriffith/scaffold-eth/blob/using-libraries-example/packages/hardhat/scripts/deploy.js#L19
-  // const yourContract = await deploy("YourContract", [], {}, {
-  //  LibraryName: **LibraryAddress**
-  // });
-
-  // todo: verification with etherscan
-  // Verification
-  // if (chainId !== "31337") {
-  //   try {
-  //     console.log(" 🎫 Verifing Contract on Etherscan... ");
-  //     await sleep(5000); // wait 5 seconds for deployment to propagate
-  //     await run("verify:verify", {
-  //       address: ExternalContract.address,
-  //       contract:
-  //         "contracts/ExternalContract.sol:ExternalContract",
-  //       contractArguments: [],
-  //     });
-  //   } catch (error) {
-  //     console.log("⚠️ Contract Verification Failed: ", error);
-  //   }
-  // }
 };
-
-// function sleep(ms) {
-//   return new Promise((resolve) => setTimeout(resolve, ms));
-// }
 
 module.exports.tags = ["ExternalContract"];
