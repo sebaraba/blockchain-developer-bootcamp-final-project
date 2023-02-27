7b0782c28e0c9f87492ba49a0a7123eade2374d5
sebaraba <rabasebastians@gmail.com>
2023-02-27 22:36:09 +0200

    fix(external-contract): tokenomics and retrieve method

--- packages/hardhat/contracts/ExternalContract.sol
+++ packages/hardhat/contracts/ExternalContract.sol
@@ -9,16 +9,28 @@ contract ExternalContract is Ownable {
   bool public completed;
   address private ownerAddres;
   uint256 public constant threshold = 1 ether;
-  uint256 public cropDeadline = block.timestamp + 5 minutes;
+  uint256 public cropDeadline = block.timestamp + 10 minutes;
+  uint256 public timeUnit = 2 minutes;
   uint256 public balance = 0;
   uint256 public firstDownpayment = 0;
   uint256 public secondDownpayment = 0;
   uint256 public thirdDownpayment = 0;
   uint256 public finalPayment = 0;
 
+  uint256 public initialContractValue = 0;
   //Events
-  event FundsWithdraw(address indexed sender, uint256 amount, string paymentBatch);
-  event CompletedContract(uint256 totalBalance, uint256 firstDownpayment, uint256 secondDownpayment, uint256 thirdDownpayment, uint256 finalPayment);
+  event FundsWithdraw(
+    address indexed sender,
+    uint256 amount,
+    string paymentBatch
+    );
+  event CompletedContract(
+    uint256 totalBalance,
+    uint256 firstDownpayment,
+    uint256 secondDownpayment,
+    uint256 thirdDownpayment,
+    uint256 finalPayment
+    );
 
   modifier noReentrant() {
     require(!locked, "No re-entrancy");
@@ -33,6 +45,7 @@ contract ExternalContract is Ownable {
 
   function complete() public payable noReentrant() returns(bool) {
     balance = balance + msg.value;
+    initialContractValue = initialContractValue + msg.value;
     firstDownpayment = balance * 1/10;
     secondDownpayment = balance * 3/20;
     thirdDownpayment = balance * 1/4;
@@ -44,28 +57,36 @@ contract ExternalContract is Ownable {
   }
 
   function retrieveFunds() public payable noReentrant() {
-    uint256 currentTime = cropDeadline / 5;
-    if (currentTime <= cropDeadline * 1/5) {
-        payable(ownerAddres).transfer(firstDownpayment);
+    uint256 currentTime = block.timestamp;
+    if (currentTime <= cropDeadline - timeUnit * 4 ) {
         emit FundsWithdraw(ownerAddres, firstDownpayment, 'FirstDownpayment');
+        payable(ownerAddres).transfer(firstDownpayment);
+        balance = balance - firstDownpayment;
         firstDownpayment = 0;
-    }
-    if (currentTime <= cropDeadline * 2/5 ) {
+    } else if (currentTime <= cropDeadline - timeUnit * 2) {
+        emit FundsWithdraw(ownerAddres, 1, 'SecondDownpayment');
         uint256 totalAmount = firstDownpayment + secondDownpayment;
         payable(ownerAddres).transfer(totalAmount);
-        emit FundsWithdraw(ownerAddres, totalAmount, 'SecondDownpayment');
+        balance = balance - totalAmount;
+        firstDownpayment = 0;
         secondDownpayment = 0;
-    }
-    if (currentTime <= cropDeadline * 4/5) {
+    } else if (currentTime <= cropDeadline - timeUnit) {
+        emit FundsWithdraw(ownerAddres, 2, 'ThirdDownpayment');
         uint256 totalAmount = firstDownpayment + secondDownpayment + thirdDownpayment;
         payable(ownerAddres).transfer(totalAmount);
-        emit FundsWithdraw(ownerAddres, totalAmount, 'ThirdDownpayment');
+        balance = balance - totalAmount;
+        firstDownpayment = 0;
+        secondDownpayment = 0;
         thirdDownpayment = 0;
-    }
-    if (currentTime >= cropDeadline) {
-        payable(ownerAddres).transfer(finalPayment);
-        emit FundsWithdraw(ownerAddres, finalPayment, 'FinalPayment');
+    } else if (currentTime >= cropDeadline) {
+        uint256 totalAmount = finalPayment + firstDownpayment + secondDownpayment + thirdDownpayment;
+        emit FundsWithdraw(ownerAddres, totalAmount, 'FinalPayment');
+        payable(ownerAddres).transfer(totalAmount);
+        balance = balance - totalAmount;
+        firstDownpayment = 0;
+        secondDownpayment = 0;
+        thirdDownpayment = 0;
+        finalPayment = 0;
     }
   }
-
 }
